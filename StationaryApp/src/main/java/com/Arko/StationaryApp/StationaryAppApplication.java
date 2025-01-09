4c9f2c45ab1bb82b1ead1e32b047af33189dac71
ArkoOf22 <arkodeepkoley123@gmail.com>
2025-01-09 12:50:28 +0530

    adding the logic and stuff

---
+++ StationaryApp/src/main/java/com/Arko/StationaryApp/StationaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.Arko.StationaryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationaryAppApplication.class, args);
+	}
+
+}
---
+++ StationaryApp/src/test/java/com/Arko/StationaryApp/StationaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arko.StationaryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
